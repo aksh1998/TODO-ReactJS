c0b5de30c668a7d24c6b6c59e24bf5e4b54239c9
rathiakshay <rathi.akshay.2108@gmail.com>
2021-01-12 23:11:18 +0530

    Updated form input for null value

--- src/Component/Formbox.js
+++ src/Component/Formbox.js
@@ -6,7 +6,7 @@ const Formbox = ({ saveTodo, inputText, setInputText, setStatus }) => {
         setInputText(e.target.value);
     }
 
-    const onSubmitHandler = (e) => {
+    function onSubmitHandler(e) {
         saveTodo(inputText);
         e.preventDefault();
     }
@@ -16,25 +16,25 @@ const Formbox = ({ saveTodo, inputText, setInputText, setStatus }) => {
     }
 
     return (
-            <form class="form-inline">
-                <div class="form-group">
-                    <input type="text" className="todo-input" value={inputText} onChange={inputTextHandler} />
+        <form class="form-inline">
+            <div class="form-group">
+                <input type="text" className="todo-input" value={inputText} onChange={inputTextHandler} />
+            </div>
+            <div class="form-group">
+                <button className="todo-button" type="submit" onClick={e => (!inputText) ? e.preventDefault() : onSubmitHandler(e)}>
+                    <i className="fas fa-plus-square"></i>
+                </button>
+            </div>
+            <div class="form-group">
+                <div className="select">
+                    <select name="todos" className="filter-todo" onChange={onDropdownChange}>
+                        <option value="all">All</option>
+                        <option value="completed">Completed</option>
+                        <option value="uncompleted">Uncompleted</option>
+                    </select>
                 </div>
-                <div class="form-group">
-                    <button className="todo-button" type="submit" onClick={onSubmitHandler}>
-                        <i className="fas fa-plus-square"></i>
-                    </button>
-                </div>
-                <div class="form-group">
-                    <div className="select">
-                        <select name="todos" className="filter-todo" onChange={onDropdownChange}>
-                            <option value="all">All</option>
-                            <option value="completed">Completed</option>
-                            <option value="uncompleted">Uncompleted</option>
-                        </select>
-                    </div>
-                </div>
-            </form>
+            </div>
+        </form>
     );
 }
 
